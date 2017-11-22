753da446f7291e56d6d651383f87c42b791fb0d6
zhaobe <zhaoben777@gmail.com>
2017-11-22 00:05:45 -0800

    Removed return type in maxNum to clarify output.

--- function.go
+++ function.go
@@ -11,24 +11,18 @@ func main() {
   rand.Seed(time.Now().UTC().UnixNano())
   var a int = randInt(50, 100)
   var b int = randInt(50, 100)
-  fmt.Println(maxNum(a, b))
+  
+  maxNum(a,b)
 }
 
 func randInt(min int, max int) int {
   return min + rand.Intn(max - min)
 }
 
-func maxNum(n1 int, n2 int) int {
-  var res int
-  var resStr string
-  
+func maxNum(n1 int, n2 int) {
   if (n1 > n2) {
-    res = n1
-    resStr = "A is greater than B"
+    fmt.Printf("(A: %d) is greater than (B: %d)\n", n1, n2)
   } else {
-    res = n2
-    resStr = "B is greater than A"
+    fmt.Printf("(B: %d) is greater than (A: %d)\n", n2, n1)
   }
-  
-  return res
 }
