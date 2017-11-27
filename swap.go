f0e43d41877ad60f7b4a6213e9975c40c2e6aeee
zhaobe <zhaoben777@gmail.com>
2017-11-26 22:04:58 -0800

    Adding a swap without using a temp variable.

---
+++ swap.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// swapping without temp variable
+func main() {
+  var a int = 3
+  var b int = 6
+  
+  fmt.Printf("Before swap: a = %d, b = %d \n", a, b)
+  a = a + b
+  b = a - b
+  a = a - b
+  
+  fmt.Printf("After swap: a = %d, b = %d \n", a, b)
+}
