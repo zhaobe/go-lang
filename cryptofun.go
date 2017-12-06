88fd6304e5c26fb9b75041c8fcfc31b85fd4670a
zhaobe <zhaoben777@gmail.com>
2017-12-05 23:59:55 -0800

    Working on encrypt function.

--- cryptofun.go
+++ cryptofun.go
@@ -4,15 +4,24 @@ import (
   "fmt"
   "bufio"
   "os"
-//  "crypto/aes"
-//  "crypto/cipher"
-//  "crypto/rand"
-//  "crypto/base64"
+  "crypto/aes"
+  "crypto/cipher"
+  "crypto/rand"
+  "encoding/base64"
 )
 
 func main() {
   reader := bufio.NewReader(os.Stdin)
   fmt.Print("Enter text: ")
   text, _ := reader.ReadString('\n')
-  fmt.Println(text)
+  fmt.Println("Original text: ", text)
+  
+  key := []byte("thisisthekey")
+  encText := encrypt(text, key)
+  fmt.Println(encText)
+}
+
+func encrypt(text string, key []byte) string {
+  plain := []byte(text)
+  
 }
