e219444d958d98ff55932035eafb8b97fa57997e
zhaobe <zhaoben777@gmail.com>
2017-12-18 00:35:23 -0800

    Duplicated findMatch function, but still need concurrency.

--- hashcollider.go
+++ hashcollider.go
@@ -8,7 +8,7 @@ import (
 
 // const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 // const letters = "abcdefghijklmnopqrstuvwxyz"
-const letters = "abc"
+const letters = "abcdef"
 
 func randStr(n int) string {
 	b := make([]byte, n)
@@ -36,14 +36,37 @@ func compare() {
     fmt.Printf("compare function took %s\n\n", elapsed)
 }
 
+func findMatch(userInput string, strLen int) string {
+	start := time.Now()
+
+	// while loop until match
+	for {
+
+		a := randStr(strLen)
+
+		if a ==  userInput {
+			fmt.Println("A match was found: ", a)
+			break
+		}
+	}
+	elapsed := time.Since(start)
+    fmt.Printf("compare function took %s\n\n", elapsed)
+    return userInput
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	var userInput string
+	var strLen int
+	
+	fmt.Println("Enter the desired string len (e.g. abcabcab is length of 8): ")
+	fmt.Scanln(&strLen)
+
+	fmt.Println("From the " + letters + " pool, please enter a combination of letters you would like to match: ")
 	fmt.Scanln(&userInput)
-	if (userInput == go compare()) {
-		fmt.Println("a match was found")
-	}
+
+	findMatch(userInput, strLen)
 
 	// go compare()
 	// go compare()
