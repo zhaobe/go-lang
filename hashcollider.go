9afedd3f8a80367298c587d55734e4c64d5c9df7
zhaobe <zhaoben777@gmail.com>
2017-12-19 22:43:55 -0800

    Adding some comments and new printouts for compare function.

--- hashcollider.go
+++ hashcollider.go
@@ -18,24 +18,30 @@ func randStr(n int) string {
 	return string(b)
 }
 
-func compare() {
+// This function generates one hash for A and then tries to find a match for B
+func compare(strLen int) {
 	start := time.Now()
 
-	a := randStr(8)
+	a := randStr(strLen)
+	var counter int = 0
+	
 	// while loop until match
 	for {
-
-		b := randStr(8)
+		counter++
+		b := randStr(strLen)
+		// fmt.Println("a: ", a, "\tb: ", b)
 
 		if a == b {
-			fmt.Println("a: ", a, "\tb: ", b)
+			fmt.Println("This was the matching sequence \ta: ", a, "\tb: ", b)
 			break
 		}
 	}
 	elapsed := time.Since(start)
     fmt.Printf("compare function took %s\n\n", elapsed)
+    fmt.Printf("and it generated %d hashes to find this match\n", counter)
 }
 
+// This function requires user input
 func findMatch(userInput string, strLen int) string {
 	start := time.Now()
 
@@ -50,6 +56,7 @@ func findMatch(userInput string, strLen int) string {
 		}
 	}
 	elapsed := time.Since(start)
+	time.Sleep(250 * time.Millisecond)
     fmt.Printf("compare function took %s\n\n", elapsed)
     return userInput
 }
@@ -57,16 +64,18 @@ func findMatch(userInput string, strLen int) string {
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	var userInput string
-	var strLen int
+	// var userInput string
+	// var strLen int
 	
-	fmt.Println("Enter the desired string len (e.g. abcabcab is length of 8): ")
-	fmt.Scanln(&strLen)
+	// fmt.Println("Enter the desired string len (e.g. abcabcab is length of 8): ")
+	// fmt.Scanln(&strLen)
 
-	fmt.Println("From the " + letters + " pool, please enter a combination of letters you would like to match: ")
-	fmt.Scanln(&userInput)
+	// fmt.Println("From the " + letters + " pool, please enter a combination of letters you would like to match: ")
+	// fmt.Scanln(&userInput)
 
-	findMatch(userInput, strLen)
+	// go findMatch(userInput, strLen)
+	
+	compare(3)
 
 	// go compare()
 	// go compare()
