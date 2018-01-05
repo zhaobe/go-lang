0f45303a105ee6506677db5b7e2dcebc0ea0e020
zhaobe <zhaoben777@gmail.com>
2018-01-04 23:59:32 -0800

    Adding median prototype.

--- mean-median-mode.go
+++ mean-median-mode.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 
 	lineOne := 10
-	lineTwo := [10]int{64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060}
+	lineTwo := []int{64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060}
 
 	var sum int
 	for i := 0; i < lineOne; i++ {
@@ -16,5 +17,20 @@ func main() {
 	mean := sum/lineOne
 	fmt.Println(mean)
 
-	
+	if(lineOne % 2 == 0) {
+		midOne := (len(lineTwo) / 2) - 1
+		midTwo := len(lineTwo) / 2
+		
+		sort.Ints(lineTwo)
+		// fmt.Println(lineTwo)
+		medianSum := lineTwo(midOne) + lineTwo(midTwo)
+		median := medianSum / 2
+		fmt.Println(  median)
+		
+		fmt.Println("even")
+	} else {
+		fmt.Println("odd")
+	}
+
+
 }
