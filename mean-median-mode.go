b87f7b801f2bb0d8a9d9f945efdf7169df3707eb
zhaobe <zhaoben777@gmail.com>
2018-01-08 23:57:28 -0800

    Pseudocode for finding mode, yet to finish.

--- mean-median-mode.go
+++ mean-median-mode.go
@@ -7,27 +7,39 @@ import (
 
 func mean(lineOne int, lineTwo []int) {
 	var sum int
+	// the i < lineOne could also be len(lineTwo), but since lineOne exist, we will use that
 	for i := 0; i < lineOne; i++ {
 		sum += lineTwo[i]
 	}
-	mean := sum/lineOne
+	mean := sum / lineOne
 	fmt.Println("Mean:\t", mean)
 }
 
 func median(lineOne int, lineTwo []int) {
-	if(len(lineTwo) % 2 == 0) {
+	if len(lineTwo)%2 == 0 {
 		midOne := (len(lineTwo) / 2) - 1
 		midTwo := len(lineTwo) / 2
-		
+
 		sort.Ints(lineTwo)
 		median := (lineTwo[midOne] + lineTwo[midTwo]) / 2
 		fmt.Println("Median:\t", median)
-		fmt.Println("even")
 	} else {
 		midPoint := (len(lineTwo) / 2) + 1
 		median := lineTwo[midPoint]
 		fmt.Println("Median:\t", median)
 	}
+}
+
+func mode(lineOne int, lineTwo []int) {
+	x := make(map[int]int)
+
+	for i := 0; i < lineOne; i++ {
+		var count int = 0
+		x[i] = lineTwo[i] // store key
+		x[lineTwo[i]] = count + 1 // count key	
+		fmt.Println("key: ", x[i], "\tvalue: \t", x[lineTwo[i]])
+	}
+
 }
 
 func main() {
@@ -36,4 +48,5 @@ func main() {
 	lineTwo := []int{64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060}
 	mean(lineOne, lineTwo)
 	median(lineOne, lineTwo)
+	mode(lineOne, lineTwo)
 }
